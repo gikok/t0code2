dedbf873f72f2ede32f44eb85ad9ab0c971bf3cd
gikok <giorgi.kokaia@ingka.ikea.com>
2022-07-05 11:16:57 +0000

    changes from temp

--- training/kick-off.sh
+++ training/kick-off.sh
@@ -1,10 +1,10 @@
 python train.py \
-    --dataset_name "prompts_001.parquet.gzip" \
-    --model_name_or_path "/home/transformers2/" \
+    --dataset_name "prompts2opts_001.parquet.gzip" \
+    --model_name_or_path "/home/gikok/8_epoch_model/" \
     --output_dir "/home/gikok/output" \
     --num_train_epochs 10 \
-    --per_device_train_batch_size 128 \
-    --per_device_eval_batch_size 128 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
     --freeze_encoder \
     --learning_rate 1e-5 \
     --parallelize
